c095e36c2b5dc57b6ae2e7dc51fedd885538295b
Daryna Lobkova <darinka021193@gmail.com>
2022-11-06 13:33:07 +0100

    add changes hw7

---
+++ js/task1.js
@@ -0,0 +1,7 @@
+const listItems = categories.children;
+console.log(listItems);
+
+const titleRef = document.querySelectorAll('h2')
+const arrayTitleRef = titleRef;
+arrayTitleRef.forEach(element => console.log('Category:', element));
+
