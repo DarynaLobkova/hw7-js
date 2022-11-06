3816f35c42c8811245e6d9b10cb33d9effc34318
Daryna Lobkova <darinka021193@gmail.com>
2022-11-06 14:17:41 +0100

    create task3 hw7

--- js/task2.js
+++ js/task2.js
@@ -1,37 +1,37 @@
-const ingredients = [
-  'Potato',
-  'Mushrooms',
-  'Garlik',
-  'Tomato',
-  'Parsley',
-  'Spices',
-];
-const listRef = document.querySelector("#ingredients");
-console.log(listRef)
+// const ingredients = [
+//   'Potato',
+//   'Mushrooms',
+//   'Garlik',
+//   'Tomato',
+//   'Parsley',
+//   'Spices',
+// ];
+// const listRef = document.querySelector("#ingredients");
+// console.log(listRef)
 
-const itemRef1 = document.createElement("li");
-itemRef1.textContent = "Potato";
-console.log(itemRef1)
+// const itemRef1 = document.createElement("li");
+// itemRef1.textContent = "Potato";
 
-const itemRef2 = document.createElement("li");
-itemRef2.textContent = "Mushrooms";
-console.log(itemRef2)
 
-const itemRef3 = document.createElement("li");
-itemRef3.textContent = "Garlik";
-console.log(itemRef3)
+// const itemRef2 = document.createElement("li");
+// itemRef2.textContent = "Mushrooms";
 
-const itemRef4 = document.createElement("li");
-itemRef4.textContent = "Tomato";
-console.log(itemRef4)
 
-const itemRef5 = document.createElement("li");
-itemRef5.textContent = "Parsley";
-console.log(itemRef5)
+// const itemRef3 = document.createElement("li");
+// itemRef3.textContent = "Garlik";
 
-const itemRef6 = document.createElement("li");
-itemRef6.textContent = "Spices";
-console.log(itemRef6)
 
-listRef.append(itemRef1,itemRef2,itemRef3,itemRef4,itemRef5,itemRef6)
+// const itemRef4 = document.createElement("li");
+// itemRef4.textContent = "Tomato";
+
+
+// const itemRef5 = document.createElement("li");
+// itemRef5.textContent = "Parsley";
+
+
+// const itemRef6 = document.createElement("li");
+// itemRef6.textContent = "Spices";
+
+
+// listRef.append(itemRef1,itemRef2,itemRef3,itemRef4,itemRef5,itemRef6)
 
