85f622e188d505691710e2cf1e070b17c270cd91
Daryna Lobkova <darinka021193@gmail.com>
2022-11-08 13:48:14 +0100

    add changes at task4 hw7

--- js/task4.js
+++ js/task4.js
@@ -1,27 +1,16 @@
-// Счетчик состоит из спана и кнопок, которые должны увеличивать и уменьшать
-//  значение счетчика на 1.
 
-// Создай переменную counterValue в которой будет хранится текущее значение счетчика.
-// Создай функции increment и decrement для увеличения и уменьшения значения счетчика
-// Добавь слушатели кликов на кнопки, вызовы функций и обновление интерфейса
-// <div id="counter">
-//   <button type="button" data-action="decrement">-1</button>
-//   <span id="value">0</span>
-//   <button type="button" data-action="increment">+1</button>
-// </div>
-
-
-// const counterValue = document.querySelector("#value");
-// console.log(counterValue)
-
-
-// const btnDecrement = document.querySelector('[data-action = decrement]');
-// btnDecrement.addEventListener('click', () => {
-   
+let counterValue = 0;
+const btnDecrement = document.querySelector('[data-action = decrement]');
+btnDecrement.addEventListener('click', () => {
+ updateDisplay( -- counterValue);
   
-// })
-// const btnIncrement = document.querySelector('[data-action = increment]');
-// btnIncrement.addEventListener('click', () => {
+})
+const btnIncrement = document.querySelector('[data-action = increment]');
+btnIncrement.addEventListener('click', () => {
+    updateDisplay( ++ counterValue)
  
-// })
+})
 
+function updateDisplay(val) {
+    document.getElementById("value").innerHTML = val;
+}
