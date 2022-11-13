933f6cfa8c6efabf10a8a77166d2b0490fb0881f
Daryna Lobkova <darinka021193@gmail.com>
2022-11-13 11:47:57 +0100

    add changes at hw7 task 5

--- js/task3.js
+++ js/task3.js
@@ -1,33 +1,33 @@
-// const images = [
-//   {
-//     url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-//     alt: 'White and Black Long Fur Cat',
-//   },
-//   {
-//     url: 'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-//     alt: 'Orange and White Koi Fish Near Yellow Koi Fish',
-//   },
-//   {
-//     url: 'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-//     alt: 'Group of Horses Running',
-//   },
-// ];
+const images = [
+  {
+    url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    alt: 'White and Black Long Fur Cat',
+  },
+  {
+    url: 'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    alt: 'Orange and White Koi Fish Near Yellow Koi Fish',
+  },
+  {
+    url: 'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    alt: 'Group of Horses Running',
+  },
+];
 
-// const listRef = document.querySelector("#gallery");
-// console.log(listRef)
+const listRef = document.querySelector("#gallery");
+console.log(listRef)
 
-// const image1 = document.createElement("img");
-// image1.src = "https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260";
-// image1.alt = "White and Black Long Fur Cat";
-
-
-// const image2 = document.createElement("img");
-// image2.src = 'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-// image2.alt = 'Orange and White Koi Fish Near Yellow Koi Fish';
+const image1 = document.createElement("img");
+image1.src = "https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260";
+image1.alt = "White and Black Long Fur Cat";
 
 
-// const image3 = document.createElement("img");
-// image3.src = 'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-// image3.alt = 'Group of Horses Running';
+const image2 = document.createElement("img");
+image2.src = 'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+image2.alt = 'Orange and White Koi Fish Near Yellow Koi Fish';
 
-// listRef.append(image1,image2,image3)
+
+const image3 = document.createElement("img");
+image3.src = 'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+image3.alt = 'Group of Horses Running';
+
+listRef.append(image1,image2,image3)
