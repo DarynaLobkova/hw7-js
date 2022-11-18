2132735e0e903ef7c961dcc2014c4ace1d005926
Daryna Lobkova <darinka021193@gmail.com>
2022-11-18 12:21:51 +0100

    add changes in task 4

--- js/task4.js
+++ js/task4.js
@@ -1,16 +1,24 @@
 
-let counterValue = 0;
-const btnDecrement = document.querySelector('[data-action = decrement]');
-btnDecrement.addEventListener('click', () => {
- updateDisplay( -- counterValue);
-  
-})
-const btnIncrement = document.querySelector('[data-action = increment]');
-btnIncrement.addEventListener('click', () => {
-    updateDisplay( ++ counterValue)
- 
-})
+let value = 0;
 
-function updateDisplay(val) {
-    document.getElementById("value").innerHTML = val;
+const btnDecrement = document.querySelector('[data-action = decrement]');
+const btnIncrement = document.querySelector('[data-action = increment]');
+const htmlValue = document.querySelector('#value')
+
+
+function decrement() {
+    value -= 1
+    render();
 }
+
+function increment() {
+    value += 1  
+    render();
+}
+
+function render() {
+    htmlValue.textContent = value;
+}
+
+btnDecrement.addEventListener('click', decrement);
+btnIncrement.addEventListener('click', increment);
